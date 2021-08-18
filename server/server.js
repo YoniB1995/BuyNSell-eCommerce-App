abff2604119025cc6f2e70eb8dfecc85ddcc0bab
Yoni <yonatansamfisher@gmail.com>
2021-08-18 17:44:26 +0300

    updated heroku deployment

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const app = express();
 const connectDB = require('./db')
@@ -27,11 +28,16 @@ app.use('/api/private',privateAuth)
 // Error Handler 
 app.use(errorHandler)
 
-app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
 
 if (process.env.NODE_ENV === 'production'){ // NODE_ENV משתנה סביבה מובנה
     app.use(express.static(path.join(__dirname,'../client/build')))
     app.get('*',(req,res)=>{
         res.sendFile(path.join(__dirname, '../client/build','index.html'))
     });
+} else {
+    app.get('/', (req,res) =>{
+        res.send("Api running")
+    })
 }
+
+app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
