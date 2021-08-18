6c356aff1f241426f1be01d36ba9766a0a923b40
Yoni <yonatansamfisher@gmail.com>
2021-08-18 17:09:48 +0300

    updated backend

--- server/server.js
+++ server/server.js
@@ -3,19 +3,22 @@ const express = require('express');
 const app = express();
 const connectDB = require('./db')
 const errorHandler = require('./middleware/error')
+const PORT = process.env.PORT || 5000
 
 const cors = require('cors');
 const productRoutes = require('./routes/productRoutes');
 const userAuth = require('./routes/auth');
 const privateAuth = require('./routes/private')
 
+app.use(express.json()); // JSON יכולת לקרוא ולהציג מידע מ
+app.use(express.urlencoded({extended:true})); // params יכולת לשלוף מידע מ
+app.use(cors());
+
+
 connectDB.on('error',()=>{
     console.log('error')
 })
 
-app.use(express.json()); // JSON יכולת לקרוא ולהציג מידע מ
-app.use(express.urlencoded({extended:true})); // params יכולת לשלוף מידע מ
-app.use(cors());
 
 app.use('/products',productRoutes)
 app.use('/userauth',userAuth)
@@ -24,6 +27,11 @@ app.use('/api/private',privateAuth)
 // Error Handler 
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000
-
 app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
+
+if (process.env.NODE_ENV === 'production'){ // NODE_ENV משתנה סביבה מובנה
+    app.use(express.static(path.join(__dirname,'../client/build')))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname, '../client/build','index.html'))
+    });
+}
