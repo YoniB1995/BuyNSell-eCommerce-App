dc2d9c50fcffb52943b194ed0ff47d63180ef85a
Yoni <yonatansamfisher@gmail.com>
2021-09-01 01:28:08 +0300

    updated secret

--- server/middleware/token.js
+++ server/middleware/token.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 exports.genToken = (userId) => {
-  let token = jwt.sign({ id: userId }, process.env.JWT_KEY, {
+  let token = jwt.sign({ id: userId }, "secret", {
     expiresIn: process.env.JWT_EXPIRE,
   });
   return token;
